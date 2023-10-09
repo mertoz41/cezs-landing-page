a163115fa3408e34f054906853408c73197240c2
mert <mertozka41@gmail.com>
2023-10-08 21:46:47 -0400

    app initated with chakra

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@chakra-ui/react";
+// import PosterContainer from "@/components/PosterContainer";
+// import Estimate from "@/components/Estimate";
+function Home() {
+  return (
+    <Box w={"100vw"} backgroundColor="red" h="100vh"> 
+      {/* <PosterContainer />
+      <Estimate /> */}
+    </Box>
+  );
+}
+
+export default Home;
